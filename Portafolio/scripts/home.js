5198b1ee221e875841b76af7426b9c9ca56744b9
Ariel Silva <51482182+Alwexis@users.noreply.github.com>
2022-05-28 16:23:02 -0400

    Cambios en el HTML, JavaScript y CSS
    Añadí la sección de información personal, con sus respectivas opciones para ver.

--- Portafolio/scripts/home.js
+++ Portafolio/scripts/home.js
@@ -1,4 +1,45 @@
 function actualizarDatos() {
     var edad = new Date().getMonth() >= 11 && new Date().getDay() >= 23 ? new Date().getFullYear() - 2001 : new Date().getFullYear() - 2002
-    document.getElementById("personalInfoP").textContent = document.getElementById("personalInfoP").textContent.replace("{edad}", edad)
+    document.getElementById("personalInfoEdad").textContent = document.getElementById("personalInfoEdad").textContent.replace("{edad}", edad)
+}
+
+function loadExtendedInfo(type) {
+    switch (type) {
+        case "personal":
+            if (document.getElementsByClassName("active")[0] != undefined && !document.getElementById("personalInfo0").classList.contains("active")) {
+                if (document.getElementById("personalInfo1").classList.contains("active")) {
+                    document.getElementById("secondExtendedInfo").style.display = "none";
+                } else if (document.getElementById("personalInfo2").classList.contains("active")) {
+                    document.getElementById("thirdExtendedInfo").style.display = "none";
+                }
+                document.getElementsByClassName("active")[0].classList.remove("active");
+            }
+            document.getElementById("firstExtendedInfo").style.display = "flex";
+            document.getElementById("personalInfo0").classList.add("active");
+            break;
+        case "intereses":
+            if (document.getElementsByClassName("active")[0] != undefined && !document.getElementById("personalInfo1").classList.contains("active")) {
+                if (document.getElementById("personalInfo0").classList.contains("active")) {
+                    document.getElementById("firstExtendedInfo").style.display = "none";
+                } else if (document.getElementById("personalInfo2").classList.contains("active")) {
+                    document.getElementById("thirdExtendedInfo").style.display = "none";
+                }
+                document.getElementsByClassName("active")[0].classList.remove("active");
+            }
+            document.getElementById("secondExtendedInfo").style.display = "flex";
+            document.getElementById("personalInfo1").classList.add("active");
+            break;
+        case "laboral":
+            if (document.getElementsByClassName("active")[0] != undefined && !document.getElementById("personalInfo2").classList.contains("active")) {
+                if (document.getElementById("personalInfo0").classList.contains("active")) {
+                    document.getElementById("firstExtendedInfo").style.display = "none";
+                } else if (document.getElementById("personalInfo1").classList.contains("active")) {
+                    document.getElementById("secondExtendedInfo").style.display = "none";
+                }
+                document.getElementsByClassName("active")[0].classList.remove("active");
+            }
+            document.getElementById("thirdExtendedInfo").style.display = "flex";
+            document.getElementById("personalInfo2").classList.add("active");
+            break;
+    }
 }
